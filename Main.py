29fd8ca502b7489f694a40a320f6783029be1a1c
pacostas0102 <pacostas0102@gmail.com>
2025-02-02 07:47:23 -0500

    concatenado completo de varianzas

--- Main.py
+++ Main.py
@@ -47,6 +47,8 @@ transformed_data = transform_data(raw_data)
 # Guardar resultado usando la función load_to_excel
 load_to_excel(transformed_data, output_path)
 
+
+
 #-------------------------------------------------------------------------LiveOffice TR && BB Reports----------------------------------------------------------------------------------------------
 
 
@@ -56,45 +58,45 @@ load_to_excel_tr_bb(transformed_data_tr_bb, output_path3)
 
 #--------------------------------------------------------------------------Host Card Reports----------------------------------------------------------------------------------------------
 # Extracción de datos
-#file_list = get_excel_files(host_folder_path)
-#dataframes = []
+file_list = get_excel_files(host_folder_path)
+dataframes = []
 
-#for file_path in file_list:
-#    df_host, file_type = extract_host_data(file_path)
-#    if df_host is not None:
-#        # Transformación según el tipo de archivo
-#        if file_type == "TransactionLookup":
-#            df_host = transform_transaction_lookup(df_host)
-#        elif file_type == "rpttransactiondetailbytid":
-#            df_host = transform_rpttransactiondetailbytid(df_host)
+for file_path in file_list:
+    df_host, file_type = extract_host_data(file_path)
+    if df_host is not None:
+        # Transformación según el tipo de archivo
+        if file_type == "TransactionLookup":
+            df_host = transform_transaction_lookup(df_host)
+        elif file_type == "rpttransactiondetailbytid":
+            df_host = transform_rpttransactiondetailbytid(df_host)
         
-#        dataframes.append(df_host)
+        dataframes.append(df_host)
 
 # Combinación de datos y exportación
-#if dataframes:
-#    combined_df = pd.concat(dataframes, ignore_index=True)
-#    load_to_excel(combined_df, output_path3)
-#else:
-#    print("No se generaron datos para exportar.")
+if dataframes:
+    combined_df = pd.concat(dataframes, ignore_index=True)
+    load_to_excel(combined_df, output_path3)
+else:
+    print("No se generaron datos para exportar.")
 
 
 #--------------------------------------------------------------------------SPARK LO-HOST CONSOLIDATION ----------------------------------------------------------------------------------------------
 
-#if dataframes:
-#    # Procesar con Spark
-#    print("Iniciando el procesamiento con Spark...")
-#    result_df, result_dfHOST = process_with_spark(transformed_data, combined_df, output_path3)  
+if dataframes:
+    # Procesar con Spark
+    print("Iniciando el procesamiento con Spark...")
+    result_df, result_dfHOST = process_with_spark(transformed_data, combined_df, output_path3)  
 
     # Exportar a Excel usando la función load_to_excel
-#    load_to_excel(result_df, output_path)
+    load_to_excel(result_df, output_path)
 
     # Verificación: Chequear si el archivo se exportó exitosamente
-#    if os.path.exists(output_path3):
-#        print(f"Archivo exportado exitosamente a {output_path3}")
-#    else:
-#        print("No se pudo exportar el archivo.")
-#else:
-#    print("No se generaron datos para exportar.")
+    if os.path.exists(output_path3):
+        print(f"Archivo exportado exitosamente a {output_path3}")
+    else:
+        print("No se pudo exportar el archivo.")
+else:
+    print("No se generaron datos para exportar.")
 
 
 #-----------------------------------------------------------------------------Card Logs Extraction-------------------------------------------------------------------------------
@@ -123,15 +125,15 @@ load_to_excel_tr_bb(transformed_data_tr_bb, output_path3)
 logs_data = extract_logs(Logs_path)
 
 # Crear un DataFrame unificado
-#df_LogsUnified = pd.DataFrame()
+df_LogsUnified = pd.DataFrame()
 
 # Transformar datos de ATM
-#dfLogsATM = transform_logs(logs_data, 'ATM')
-#df_LogsUnified = pd.concat([df_LogsUnified, dfLogsATM], ignore_index=True)
+dfLogsATM = transform_logs(logs_data, 'ATM')
+df_LogsUnified = pd.concat([df_LogsUnified, dfLogsATM], ignore_index=True)
 
 # Transformar datos de Cash Advance
-#dfLogsCA = transform_logs(logs_data, 'CashAdvance')
-#df_LogsUnified = pd.concat([df_LogsUnified, dfLogsCA], ignore_index=True)
+dfLogsCA = transform_logs(logs_data, 'CashAdvance')
+df_LogsUnified = pd.concat([df_LogsUnified, dfLogsCA], ignore_index=True)
 
 # Transformar datos de Ticket Redemption
 dfLogsTR = transform_logs(logs_data, 'TicketRedemption')
@@ -141,21 +143,21 @@ dfLogsBB = transform_logs(logs_data, 'BillBreaking')
 
 # Mostrar y exportar el DataFrame unificado usando la función load_to_excel
 #print(df_LogsUnified)
-load_to_excel(dfLogsTR, output_path)
+#load_to_excel(dfLogsTR, output_path)
 
-load_to_excel(dfLogsBB, output_path3)
+#load_to_excel(dfLogsBB, output_path3)
 
 #------------------------------------------------------------------SPARK Consolidado logs------------------------------------------------------------------------------
 
 # Procesar los logs con Spark y obtener los DataFrames finales
-#result_df1, result_df2 = process_logs_with_spark(df_LogsUnified, result_df, result_dfHOST)
+result_df1, result_df2 = process_logs_with_spark(df_LogsUnified, result_df, result_dfHOST)
 
 # Guardar los resultados en un archivo Excel usando la función load_to_excel
-#with pd.ExcelWriter(output_path3, engine='openpyxl') as writer:
-#    result_df1.to_excel(writer, sheet_name='LOGSvsLO-HOST', index=False)  # Guardar df1 en 'Hoja1'
-#    result_df2.to_excel(writer, sheet_name='LOGSvsHOST', index=False)  # Guardar df2 en 'Hoja2'
+with pd.ExcelWriter(output_path3, engine='openpyxl') as writer:
+    result_df1.to_excel(writer, sheet_name='LOGSvsLO-HOST', index=False)  # Guardar df1 en 'Hoja1'
+    result_df2.to_excel(writer, sheet_name='LOGSvsHOST', index=False)  # Guardar df2 en 'Hoja2'
 
-#print(f"Archivo exportado exitosamente a {output_path3}")
+print(f"Archivo exportado exitosamente card transactions a {output_path3}")
 
 #------------------------------------------------------------------SPARK Consolidado logs vs LO (TR-BB)------------------------------------------------------------------------------
 
